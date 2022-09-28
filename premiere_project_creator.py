35a77e5d679d9ba6ac0ff09ae4165e561355d2e4
Tobias Pilarche <tobsailbot@gmail.com>
2022-09-28 12:33:01 -0300

    Add files via upload

---
+++ premiere_project_creator.py
@@ -0,0 +1,42 @@
+import os
+import easygui as eg
+import subprocess
+import time
+
+campos = ['Project Name', 'Project Path', 'Media Folder']
+
+default = ['',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS',r'F:\VIDEO MEDIA\OBS VIDEOS']
+
+# creates a window with multiple input boxes
+box = eg.multenterbox(msg='Enter the proj name and path:',title='PremierePro Project Creator',fields=campos, values=default)
+
+if not box == None:
+    print(box)
+    # pasar string del nombre del proj como interfaz UI
+    project_name = box[0]
+
+    proj_dir = box[1] + f'\\{project_name}'
+    replace_this = "proyecto_prueba"
+    lines = []
+
+    os.makedirs(proj_dir)
+
+    #open file
+    with open(r'proyecto_testing_xd.xml', mode='r',encoding='utf8') as f:
+        for line in f.readlines(): # iterate thru the lines
+            if replace_this in line: # check if is in ans in line
+                print(line)
+                line = line.replace(replace_this, project_name) # replace the line containing the and with the new line, you can change to what you want. 
+                print(line)
+            lines.append(line)
+
+
+    #write to a new file
+    with open(proj_dir +'/'+ project_name + '.prproj' , mode='w', encoding='utf8') as new_f:
+        new_f.writelines(lines)
+
+    time.sleep(0.5)
+
+    # open the media folders
+    subprocess.Popen(f'explorer "{box[2]}"')
+    os.startfile(proj_dir +'/'+ project_name + '.prproj' )
